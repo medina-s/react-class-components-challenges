1e5560d628091ff1be2050d919242a0a507767d0
Medina88 <Msydyka@iu.edu>
2021-08-28 01:15:10 -0400

    Added search term input

--- src/Components/Search/SearchIndex.js
+++ src/Components/Search/SearchIndex.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Input} from 'reactstrap';
- 
-const SearchIndex extends Component() {
-   this.state = {
-     things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards']
-   }
- }
 
- function searchFunction() {
- }
+class SearchIndex extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards']
+    }
+    this.state = {term : ''}
+    this.searchFunction=this.searchFunction.bind(this)
+  }
+  
+  searchFunction(e){
 
- render() {
-     <div>
-       <Input placeholder='Search Here' />
-       <h3>Results:</h3>
-     </div>
- }
+    console.log(this.state.term)
+  }
 
- 
-export SearchIndex;
+  handleSearchTerm(e){
+    this.setState({term : e.target.value})
+  }
+  
+  render() {
+    return(
+      <div>
+        
+          <Input placeholder='Search Here' name="text" onChange={(e) => { this.handleSearchTerm(e) }}/>
+          <button type="submit" onClick={(e)=>this.searchFunction(e)}>Search</button>
+      <h3>Results:</h3>
+    </div>
+    );
+  }
+}
+
+
+export default SearchIndex;
